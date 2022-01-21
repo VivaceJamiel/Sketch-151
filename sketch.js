9cd3d1c1b8e2045ea623dfbf84d1e315d5a7c7af
Jamiel <jimpoy2@gmail.com>
2022-01-20 19:36:13 -0600

    added prespective parts

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ var x = 500;
 console.log(x);
 
 x = 10;
-speed = 1;
-speed2 = 1;
+var speed = 1;
+var speed2 = 1;
 
 circleDims = [10, 20, 100];
 z = 0;
@@ -16,14 +16,24 @@ z = 0;
 function draw() {
     clear();
 
-    for (var i = 0; i < 31; i++) {
+    rect((windowWidth/2)-120,  0, 240, windowHeight);
+    rect((windowWidth/2)-105,  0, 210, windowHeight);
+    rect((windowWidth/2)-100,  0, 200, windowHeight);
+    rect((windowWidth/2)-95,   0, 190, windowHeight);
+    rect((windowWidth/2)-92.5, 0, 185, windowHeight);
+    rect((windowWidth/2)-90,   0, 180, windowHeight);
+
+    
+    for (var i = 0; i < 38; i++) {
         line(i * 50, 0, i * 50, windowHeight);
         line(0, i * 50, windowWidth, i * 50);   
     }
 
     // Rectangle in middle
+    fill(75, 75, 150);
     rect((windowWidth/2)-90, 0, 180, windowHeight);
 
+    fill(255, 255, 255);
     // Left Triangles
     triangle(windowWidth/2, windowHeight/2, 0, windowHeight-1, 0, 1);
     triangle(windowWidth/2, windowHeight/2, 0, windowHeight-2, 0, 2);
